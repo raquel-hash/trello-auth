49073f36d7aae0a26465ee9b8448f64fb7b1f141
Raquel <quelcaraquel1@gmail.com>
2024-03-11 09:27:56 -0400

    Flujo de login

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  apiUrl = environment.API_URL;
+
+  login(email: string, password: string) {
+    return this.http.post(`${this.apiUrl}/api/v1/auth/login`, {
+      email,
+      password,
+    });
+  }
+}
