e864572c88b121ef11fd0f4ebf8175ea4f079a91
Raquel <quelcaraquel1@gmail.com>
2024-03-11 09:59:13 -0400

    Conectar login a API

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -2,13 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
