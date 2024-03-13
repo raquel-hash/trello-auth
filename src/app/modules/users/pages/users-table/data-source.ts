c5c027f1a3ad6b1c2ac083c2281861240da1c31b
Raquel <quelcaraquel1@gmail.com>
2024-03-13 08:55:48 -0400

    Obtener users

--- src/app/modules/users/pages/users-table/data-source.ts
+++ src/app/modules/users/pages/users-table/data-source.ts
@@ -1,20 +1,19 @@
-import { DataSource } from '@angular/cdk/collections';
+import { CollectionViewer, DataSource } from '@angular/cdk/collections';
+import { User } from '@models/user.model';
 import { BehaviorSubject, Observable } from 'rxjs';
 
-export class DataSourceUser extends DataSource<any[]> {
+export class DataSourceUser extends DataSource<User> {
+  data = new BehaviorSubject<User[]>([]);
+  originalData: User[] = [];
 
-  data = new BehaviorSubject<any[]>([]);
-  originalData: any[]= [];
-
-  connect(): Observable<any[]> {
+  connect(): Observable<User[]> {
     return this.data;
   }
 
-  init(data: any[]) {
+  init(data: User[]) {
     this.originalData = data;
     this.data.next(data);
   }
 
-  disconnect() { }
-
+  disconnect() {}
 }
