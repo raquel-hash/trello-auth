a63ea9c422f19b0170524af9bf0d8bef44cf2ec0
Raquel <quelcaraquel1@gmail.com>
2024-04-01 14:48:06 -0400

    Detalle de un board

--- src/app/modules/boards/pages/board/board.component.ts
+++ src/app/modules/boards/pages/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   CdkDragDrop,
   moveItemInArray,
@@ -8,6 +8,10 @@ import { Dialog } from '@angular/cdk/dialog';
 import { TodoDialogComponent } from '@boards/components/todo-dialog/todo-dialog.component';
 
 import { ToDo, Column } from '@models/todo.model';
+import { ActivatedRoute } from '@angular/router';
+import { BoardService } from '@services/board.service';
+import { Board } from '@models/board.model';
+import { Card } from '@models/card.model';
 
 @Component({
   selector: 'app-board',
@@ -23,48 +27,29 @@ import { ToDo, Column } from '@models/todo.model';
     `,
   ],
 })
-export class BoardComponent {
-  columns: Column[] = [
-    {
-      title: 'ToDo',
-      todos: [
-        {
-          id: '1',
-          title: 'Make dishes',
-        },
-        {
-          id: '2',
-          title: 'Buy a unicorn',
-        },
-      ],
-    },
-    {
-      title: 'Doing',
-      todos: [
-        {
-          id: '3',
-          title: 'Watch Angular Path in Platzi',
-        },
-      ],
-    },
-    {
-      title: 'Done',
-      todos: [
-        {
-          id: '4',
-          title: 'Play video games',
-        },
-      ],
-    },
-  ];
+export class BoardComponent implements OnInit {
+  board: Board | null = null;
 
   todos: ToDo[] = [];
   doing: ToDo[] = [];
   done: ToDo[] = [];
 
-  constructor(private dialog: Dialog) {}
+  constructor(
+    private dialog: Dialog,
+    private route: ActivatedRoute,
+    private boardService: BoardService
+  ) {}
 
-  drop(event: CdkDragDrop<ToDo[]>) {
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      if (id) {
+        this.getBoard(id);
+      }
+    });
+  }
+
+  drop(event: CdkDragDrop<Card[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(
         event.container.data,
@@ -82,22 +67,28 @@ export class BoardComponent {
   }
 
   addColumn() {
-    this.columns.push({
-      title: 'New Column',
-      todos: [],
-    });
+    // this.columns.push({
+    //   title: 'New Column',
+    //   todos: [],
+    // });
   }
 
-  openDialog(todo: ToDo) {
+  openDialog(card: Card) {
     const dialogRef = this.dialog.open(TodoDialogComponent, {
       minWidth: '300px',
       maxWidth: '50%',
       data: {
-        todo: todo,
+        card: card,
       },
     });
     dialogRef.closed.subscribe((output) => {
       console.log(output);
+    });
+  }
+
+  private getBoard(id: string) {
+    this.boardService.getBoard(id).subscribe((board) => {
+      this.board = board;
     });
   }
 }
---
+++ src/app/services/board.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { checkToken } from '@interceptors/token.interceptor';
+import { Board } from '@models/board.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BoardService {
+  apiUrl = environment.API_URL;
+
+  constructor(private http: HttpClient) {}
+
+  getBoard(id: Board['id']) {
+    return this.http.get<Board>(`${this.apiUrl}/api/v1/boards/${id}`, {
+      context: checkToken(),
+    });
+  }
+}
