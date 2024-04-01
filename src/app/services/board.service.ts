5d7ba4cb34263e338474d4927cfe8d577bbcd7ea
Raquel <quelcaraquel1@gmail.com>
2024-04-01 15:47:16 -0400

    Calcular la posición

--- src/app/services/board.service.ts
+++ src/app/services/board.service.ts
@@ -9,6 +9,7 @@ import { Card } from '@models/card.model';
   providedIn: 'root',
 })
 export class BoardService {
+  bufferSpace = 65535;
   apiUrl = environment.API_URL;
 
   constructor(private http: HttpClient) {}
@@ -20,21 +21,24 @@ export class BoardService {
   }
 
   getPosition(cards: Card[], currentIndex: number) {
-    console.log(cards, currentIndex);
     if (cards.length === 1) {
-      return 'isNew';
+      return this.bufferSpace;
     }
 
     if (cards.length > 1 && currentIndex === 0) {
-      return 'is the top';
+      const onTopPosition = cards[1].position;
+      return onTopPosition / 2;
     }
     const lastIndex = cards.length - 1;
     if (cards.length > 2 && currentIndex > 0 && currentIndex < lastIndex) {
-      return 'is the middle';
+      const prevPosition = cards[currentIndex - 1].position;
+      const nextPosition = cards[currentIndex + 1].position;
+      return (prevPosition + nextPosition) / 2;
     }
 
     if (cards.length > 1 && currentIndex === lastIndex) {
-      return 'is the bottom';
+      const onBottomPosition = cards[lastIndex - 1].position;
+      return onBottomPosition + this.bufferSpace;
     }
     return 0;
   }
