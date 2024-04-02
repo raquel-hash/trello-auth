06efa6cb5f0be934af4b1e8b8efbc86e721f4157
Raquel <quelcaraquel1@gmail.com>
2024-04-02 10:53:49 -0400

    Crear un nuevo board: UI

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -7,19 +8,17 @@ import { SharedModule } from '@shared/shared.module';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './components/layout/layout.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-
+import { BoardFormComponent } from './components/board-form/board-form.component';
 
 @NgModule({
-  declarations: [
-    LayoutComponent,
-    NavbarComponent
-  ],
+  declarations: [LayoutComponent, NavbarComponent, BoardFormComponent],
   imports: [
     CommonModule,
     LayoutRoutingModule,
     OverlayModule,
     FontAwesomeModule,
-    SharedModule
-  ]
+    SharedModule,
+    ReactiveFormsModule,
+  ],
 })
-export class LayoutModule { }
+export class LayoutModule {}
